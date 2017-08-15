8f57e57ef029d4f4aeb3696188ede3c7834b803d
Your Name <you@example.com>
2017-08-15 17:49:22 +0900

    without cld3

--- app/lib/language_detector.rb
+++ app/lib/language_detector.rb
@@ -6,38 +6,15 @@ class LanguageDetector
   def initialize(text, account = nil)
     @text = text
     @account = account
-    @identifier = CLD3::NNetLanguageIdentifier.new(1, 2048)
+    #@identifier = CLD3::NNetLanguageIdentifier.new(1, 2048)
   end
 
   def to_iso_s
-    detected_language_code || default_locale
+    default_locale
   end
 
   def prepared_text
-    simplified_text.strip
-  end
-
-  private
-
-  def detected_language_code
-    result.language.to_sym if detected_language_reliable?
-  end
-
-  def result
-    @result ||= @identifier.find_language(prepared_text)
-  end
-
-  def detected_language_reliable?
-    result.reliable?
-  end
-
-  def simplified_text
-    text.dup.tap do |new_text|
-      new_text.gsub!(FetchLinkCardService::URL_PATTERN, '')
-      new_text.gsub!(Account::MENTION_RE, '')
-      new_text.gsub!(Tag::HASHTAG_RE, '')
-      new_text.gsub!(/\s+/, ' ')
-    end
+    @text
   end
 
   def default_locale
