ffd39568e95b280af04657d9e3de46ed5059fd51
Your Name <you@example.com>
2017-08-17 16:19:59 +0900

    without stream api

--- app/javascript/mastodon/stream.js
+++ app/javascript/mastodon/stream.js
@@ -1,12 +1,14 @@
 import WebSocketClient from 'websocket.js';
 
 export default function getStream(streamingAPIBaseURL, accessToken, stream, { connected, received, disconnected, reconnected }) {
-  const ws = new WebSocketClient(`${streamingAPIBaseURL}/api/v1/streaming/?access_token=${accessToken}&stream=${stream}`);
 
-  ws.onopen      = connected;
-  ws.onmessage   = e => received(JSON.parse(e.data));
-  ws.onclose     = disconnected;
-  ws.onreconnect = reconnected;
+  //const ws = new WebSocketClient(`${streamingAPIBaseURL}/api/v1/streaming/?access_token=${accessToken}&stream=${stream}`);
 
-  return ws;
+  //ws.onopen      = connected;
+  //ws.onmessage   = e => received(JSON.parse(e.data));
+  //ws.onclose     = disconnected;
+  //ws.onreconnect = reconnected;
+
+  //return ws;
+  return null;
 };
